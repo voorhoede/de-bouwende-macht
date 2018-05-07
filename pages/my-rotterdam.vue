e162e807cfc8648f30dd670c4778bc6f4ff0c2b2
bruna <brunadafonseca@gmail.com>
2018-05-07 16:58:06 +0200

    Add method that handles answers

--- pages/my-rotterdam.vue
+++ pages/my-rotterdam.vue
@@ -3,5 +3,60 @@
     <h1>
       Mijn Rotterdam
     </h1>
+
+    <button @click="$store.commit('nextQuestion')">Start Game</button>
+    <div v-if="currentQuestion">
+      <p>{{ currentQuestion.question }}</p>
+      <button v-for="(answer, index) in currentQuestion.answers" @click="handleAnswer(answer)" :key="index">{{ answer.answer }}</button>
+    </div>
+
+    <p>{{ questions }}</p>
   </section>
 </template>
+
+<script>
+
+export default {
+  computed: {
+    currentQuestion () {
+      return this.$store.state.currentQuestion
+    },
+    questions () {
+      return this.$store.state.questions
+    }
+  },
+  methods: {
+    handleAnswer(answer) {
+      this.$store.commit('increment')
+      
+      const consequence = answer.consequences[0]
+      if (consequence.question) {
+        this.$store.commit('insertQuestion', consequence)
+      } else {
+        console.log(consequence.outcome.ref)
+        // this.$store.commit('changeCity') ???
+      }
+
+      return this.$store.commit('nextQuestion')
+    }
+  }
+}
+
+
+// question
+//   answers
+//     yes
+//       question
+//       answers
+//         yes
+//           question
+//           answer
+//             outcome
+        
+//         no
+//           outcome
+    
+//     no
+//       outcome
+
+</script>
