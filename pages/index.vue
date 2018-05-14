77d7d8d03f565abfa798482a49a48973b711e27f
bruna <brunadafonseca@gmail.com>
2018-05-14 13:47:36 +0200

    Add link to game at homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <section>
-    <h1>
-      Welkom bij De Bouwende Macht
-    </h1>
+    <h1>Welkom bij De Bouwende Macht</h1>
+
+    <nuxt-link to="/my-rotterdam">Go to Game</nuxt-link>
   </section>
 </template>
