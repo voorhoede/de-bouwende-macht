b37980d35f09db371da4175fc25fce189b83b5d7
Sanne Veroude <sanne@voorhoede.nl>
2018-05-24 11:37:23 +0200

    feat(assets): add character for feedback

--- components/QuestionNotice.vue
+++ components/QuestionNotice.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="question-notice toast">
-    <img class="bob-de-bouwer" src="~static/images/bob-bouwer.svg">
+    <img class="character" src="~static/images/bob-bouwer.svg">
     <p>Er is een bouwvraag!</p>
     <button class="button-primary" @click="onClickButton()">Speel!</button>
   </div>
