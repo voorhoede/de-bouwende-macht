55e631c511b4df1452b57dc66dee2f97ae76a9f8
bruna <brunadafonseca@gmail.com>
2018-05-25 10:02:39 +0200

    Merge master, add sharing logic and button styling

--- components/ShareButton.vue
+++ components/ShareButton.vue
@@ -1,15 +1,24 @@
 <template>
-  <nuxt-link
-    class="share-button"
-    to="/share-my-rotterdam"
-  >
+  <button class="share-button" @click="share">
     <img class="share-icon" src="~static/images/share-button.svg">
-  </nuxt-link>
+  </button>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
-  
+  computed: mapState({
+    slugs: state => state.currentScenario
+  }),
+
+  methods: {
+    share () {
+      const slugs = this.slugs.join('-')
+
+      this.$router.push(`share-my-rotterdam/${slugs}`)
+    }
+  }
 }
 </script>
 
@@ -17,18 +26,64 @@ export default {
 @import '~/assets/core.css';
 
 .share-button {
-  display: block;
-  height: var(--button-size);
-  width: var(--button-size);
-  margin-bottom: var(--spacing-half);
-  background-color: white;
+  padding: 0;
+  width: 40px;
+  height: 40px;
+  opacity: 0;
   border-radius: 50%;
-  box-shadow: var(--box-shadow);
+  animation-name: bounceIn;
+  animation-duration: 1s;
+  animation-timing-function: linear;
+  animation-fill-mode: forwards;
+}
+
+@keyframes bounceIn {
+  0% {
+    opacity: 0;
+    transform: scale(.3);
+  }
+
+  50% {
+    opacity: .9;
+    transform: scale(1.1);
+  }
+
+  80% {
+    opacity: 1;
+    transform: scale(.89);
+  }
+
+  100% {
+    opacity: 1;
+    transform: scale(1) translate3d(0,0,0);
+  }
+}
+
+@keyframes glow {
+  50% {
+    transform: scale(1.3);
+  }
 }
 
 .share-icon {
-  height: var(--button-size);
-  width: var(--button-size);
+  width: 40px;
+  height: 40px;
+  animation-name: bounceIn;
+  animation-duration: 1s;
+  animation-timing-function: linear;
+  animation-fill-mode: forwards;
 }
 
+.share-button:before {
+  content: '';
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  animation-name: glow;
+  animation-duration: 2s;
+  animation-timing-function: ease-in-out;
+  animation-fill-mode: forwards;
+  animation-iteration-count: 5;
+  background-color: #fff;
+}
 </style>
