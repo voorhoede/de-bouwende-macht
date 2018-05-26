e5f70c3756f4b6bbd88142270edf9884aa413010
Sanne Veroude <sanne@voorhoede.nl>
2018-05-26 20:43:25 +0200

    chore(config): adjust meta data

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,11 +3,11 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'vers-beton',
+    title: 'De Bouwende Macht',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'De Bouwende Macht' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -16,7 +16,7 @@ module.exports = {
   /*
   ** Customize the progress bar color
   */
-  loading: { color: '#3B8070' },
+  loading: { color: '#ff738e' },
   /*
   ** Build configuration
   */
