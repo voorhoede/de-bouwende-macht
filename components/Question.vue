3d914707dcc6e0c86564d88f0e491d429c3a02b8
Jurgen Beliën <jurgen@voorhoede.nl>
2018-05-28 11:12:12 +0200

    feat(Question): make collapsible

--- components/Question.vue
+++ components/Question.vue
@@ -1,32 +1,64 @@
 <template>
-  <div class="toast card">
-    <div>
-      <p class="current-question">{{ currentQuestion.question }}</p>
-      
+  <div :class="`question toast card${(minified) ? ' minified' : ''}`">
+      <button class="minify-button" @click="toggleMinified">
+        <span class="a11y-sr-only">Minify</span>
+      </button>
+      <p class="current-question">
+        {{ currentQuestion.question }}
+      </p>
+
       <div class="answers">
-        <button class="answer-button" v-for="(answer, index) in currentQuestion.answers" @click="onClickButton(answer)" :key="index">{{ answer.label }}</button>
+        <button
+          class="answer-button"
+          v-for="(answer, index) in currentQuestion.answers"
+          :key="index"
+          @click="onClickButton(answer)"
+        >
+          {{ answer.label }}
+        </button>
       </div>
-    </div>
   </div>
 </template>
 
 <script>
 
 export default {
+  data () {
+    return { minified: false }
+  },
   props: ['currentQuestion', 'answerFeedback'],
   methods: {
     onClickButton (answer) {
       this.$emit('onAnswer', answer)
+    },
+    toggleMinified () {
+      this.minified = !this.minified
     }
   }
 }
 
 </script>
 
-<style scoped>
+<style>
 @import '~/assets/core.css';
-
+.question {
+  transition: transform .5s;
+}
 .current-question {
   margin-bottom: 2rem;
+}
+.minify-button {
+  position: absolute;
+  top: 0;
+  width: 4rem;
+  height: 2rem;
+  margin-left: -2rem;
+  transition: transform .5s;
+}
+.minified {
+  transform: translateY(calc(100% - 2rem));
+}
+.minified .minify-button {
+  transform: rotateZ(180deg);
 }
 </style>
