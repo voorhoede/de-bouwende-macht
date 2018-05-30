1bd1cddfa13d31c35da7dac4733089d8b9d45492
Sanne Veroude <veroude.sanne@gmail.com>
2018-05-30 14:18:19 +0200

    feat(question): add focus to first button on DOM update (#32)

--- components/Question.vue
+++ components/Question.vue
@@ -5,6 +5,7 @@
       
       <div class="answers">
         <button
+          :ref="'answer'"
           class="answer-button"
           v-for="(answer, index) in currentQuestion.answers"
           @click="onClickButton(answer, currentQuestion)" :key="index"
@@ -27,6 +28,9 @@ export default {
         eventAction: 'answer',
         eventLabel: currentQuestion.question,
         eventValue: answer.label
+      })
+      this.$nextTick( () => {
+        this.$refs.answer[0].focus()
       })
       this.$emit('onAnswer', answer)
     }
