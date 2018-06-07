f18f26e7c724c2fb16769f0abd67152389cb9133
Sanne Veroude <sanne@voorhoede.nl>
2018-06-07 19:48:59 +0200

    config(nuxt): update title

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'De Bouwende Macht',
+    title: 'Bouwen is Macht',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1, minimal-ui' },
